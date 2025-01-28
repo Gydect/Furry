0c7bca6355e9f93bdb6b77b14ea603ede1d6a834
Gyde <gyde5211314@gmail.com>
2025-01-28 16:51:13 +0800

    临时替换人物模板动画，修正文本内容

--- scripts/languages/furry_chinese.lua
+++ scripts/languages/furry_chinese.lua
@@ -2,12 +2,12 @@
 STRINGS.CHARACTER_TITLES.furry_yellowcat = "绒绒" -- 标题
 STRINGS.CHARACTER_NAMES.furry_yellowcat = "绒绒" -- 名字
 STRINGS.CHARACTER_DESCRIPTIONS.furry_yellowcat = "*喜欢烹饪\n*喜欢料理\n*喜欢朋友" -- 描述
-STRINGS.CHARACTER_QUOTES.furry_yellowcat = "\"会产奶的Furry\"" -- 特点
+STRINGS.CHARACTER_QUOTES.furry_yellowcat = "\"福瑞\"" -- 特点
 
 STRINGS.CHARACTERS.ZSKB_ZOMBIE = require "speech_wilson" -- 人物语言文件,可以进去自定义
 
 STRINGS.NAMES.ZSKB_ZOMBIE = "绒绒" -- 人物在游戏里面的名字
-STRINGS.SKIN_NAMES.furry_yellowcat_none = "僵尸" -- 检查界面显示的名字
+STRINGS.SKIN_NAMES.furry_yellowcat_none = "绒绒" -- 检查界面显示的名字
 
 STRINGS.CHARACTER_SURVIVABILITY.furry_yellowcat = "可爱即是正义" -- 生存几率
 
